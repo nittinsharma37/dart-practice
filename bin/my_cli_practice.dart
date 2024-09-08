d6f6cf4a7f712a8345276ff539afcc0426b587ed
nittinsharma37 <nittinsharma37@gmail.com>
2024-09-08 12:43:41 +0530

    add more methods

--- bin/my_cli_practice.dart
+++ bin/my_cli_practice.dart
@@ -1,11 +1,25 @@
+import 'package:my_cli_practice/leap.dart';
 import 'package:my_cli_practice/my_cli_practice.dart';
 
 void main() {
-  print("hello from dart cli");
+  String str = "hello from dart cli";
+  print(str);
+
+  Leap leapYearResult = Leap();
+  print(leapYearResult.leapYear(2024));
+
   List<int> arr = const [23, 5, 8, -9, 11, 11, 13, 15, 16];
   print(arr);
   Map<String, int> result1 = findLargestSmallestWithreduce(arr);
   Map<String, int> result2 = findLargestSmallest(arr);
   Map<String, int> result3 = secondSmallestAndLargestwithSet(arr);
   print("$result1\n$result2\n$result3");
+  List arrResversedresult = reverseArr(arr);
+  print("reversed arr :  $arrResversedresult");
+  List revArr = reverseArrFor(arr);
+  print(revArr);
+  String revStr = revString(str);
+  print(revStr);
+  String revWrds = revWords(str);
+  print(revWrds);
 }
--- lib/my_cli_practice.dart
+++ lib/my_cli_practice.dart
@@ -1,30 +1,5 @@
 import "dart:math";
 
-// void main() {
-//   for (int i = 0; i < 10; i++) {
-//     print('hello ${i + 1}');
-//   }
-//   Leap leap = Leap();
-
-//   print(leap.leapYear(2023));
-
-// }
-
-// class Leap{
-//   Leap();
-
-//   bool leapYear(int year){
-
-//     if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
-//       return true;
-//     }else{
-//       return false;
-//     }
-
-//   }
-
-// }
-
 Map<String, int> findLargestSmallestWithreduce(List<int> arr) {
   int smallest = arr.reduce(min);
   int largest = arr.reduce(max);
@@ -64,4 +39,36 @@ List<int> removeDuplicatesAndSort(List<int> arr) {
 
   unique.sort();
   return unique as List<int>;
+}
+
+List reverseArr(List arr) {
+  return arr.reversed.toList();
+}
+
+List reverseArrFor(List arr) {
+  List reversedArr = [];
+
+  for (int i = arr.length - 1; i >= 0; i--) {
+    reversedArr.add(arr[i]);
+  }
+  return reversedArr;
+}
+
+String revString(String str) {
+  String reversedStr = '';
+
+  for (int i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+
+  return reversedStr;
+}
+
+String revWords(String str) {
+  List<String> revWords = [];
+  List<String> words = str.split(' ');
+  for (int i = words.length - 1; i >= 0; i--) {
+    revWords.add(words[i]);
+  }
+  return revWords.join(" ");
 }
